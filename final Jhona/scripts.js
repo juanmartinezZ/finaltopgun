d8abadd6bd30a7a9f181ab8f419a7988598e379d
juanmartinezZ <48389661+juanmartinezZ@users.noreply.github.com>
2019-03-23 15:43:16 -0500

    subir los documentos

---
+++ final Jhona/scripts.js
@@ -0,0 +1,12 @@
+// web api
+
+const itm1 = document.createElement("LI")
+itm1.textContent = "Game chess"
+
+//document.querySelector
+const lista = document.querySelector("#mi-lista")
+
+
+//DESTINO.appendChild(nodoaagregar)
+
+lista.appendChild(itm1)
